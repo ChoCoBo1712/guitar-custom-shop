13728e46b85e793be72458a9f388d64a9d0ff963
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2021-09-17 17:08:55 +0300

    fixes

--- src/main/java/com/chocobo/customshop/controller/command/Command.java
+++ src/main/java/com/chocobo/customshop/controller/command/Command.java
@@ -2,6 +2,7 @@ package com.chocobo.customshop.controller.command;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+@FunctionalInterface
 public interface Command {
 
     CommandResult execute(HttpServletRequest request);
